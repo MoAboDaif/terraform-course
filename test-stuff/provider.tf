212cfb832f4a5047178722290a636bb2caa565e1
Mohamed AboDaif <MoAboDaif@Ubuntu-VSCode.localdomain>
2025-07-30 02:33:03 +0300

    modified:   test-stuff/.terraform.lock.hcl 	modified:   test-stuff/provider.tf

--- test-stuff/provider.tf
+++ test-stuff/provider.tf
@@ -5,7 +5,12 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 6.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
+  
   backend "s3" {
     bucket = "moabodaif-terraform-bucket"
     key = "terraform-course/test-stuff/terraform.tfstate"
@@ -18,5 +23,29 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket_prefix = "moabodaif-terraform-bucket-prefix"
+  bucket_prefix = "moabodaif-terraform-${terraform.workspace}-bucket-"
 }
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "workspaced" {
+  bucket = "moabodaif-terraform-${terraform.workspace}-${random_id.bucket_suffix.hex}"
+}
+
+# import {
+#   to = aws_s3_bucket.backend
+#   id = "moabodaif-terraform-bucket"
+# }
+
+# resource "aws_s3_bucket" "backend" {
+#   bucket = "moabodaif-terraform-bucket"
+# }
+
+# removed {
+#   from = aws_s3_bucket.backend
+#   lifecycle {
+#     destroy = false
+#   }
+# }
